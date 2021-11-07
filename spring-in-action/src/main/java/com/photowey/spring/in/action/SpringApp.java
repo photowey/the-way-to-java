ea563ddae1dd843e8d269f0d1d0db5bec893a36b
photowey <photowey@gmail.com>
2021-11-08 04:12:11 +0800

    chore:add spring-in-action module

---
+++ spring-in-action/src/main/java/com/photowey/spring/in/action/SpringApp.java
@@ -0,0 +1,19 @@
+package com.photowey.spring.in.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * {@code SpringApp}
+ *
+ * @author photowey
+ * @date 2021/11/08
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class SpringApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class, args);
+    }
+}
---
+++ spring-in-action/src/test/java/com/photowey/spring/in/action/SpringAppTest.java
@@ -0,0 +1,20 @@
+package com.photowey.spring.in.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * {@code SpringAppTest}
+ *
+ * @author photowey
+ * @date 2021/11/08
+ * @since 1.0.0
+ */
+@SpringBootTest
+class SpringAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
