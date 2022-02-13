648a389ac4ff7099ab5db83eb263d5bf998666ef
photowey <photowey@gmail.com>
2022-02-13 14:35:42 +0800

    feat: websocket-netty-in-action module.

---
+++ netty-in-action/websocket-netty-in-action/src/main/java/com/photowey/websocket/netty/in/action/netty/handler/WebsocketHelloHandler.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright © 2021 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.websocket.netty.in.action.netty.handler;
+
+import com.photowey.websocket.netty.in.action.netty.property.NettyProperties;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * {@code WebsocketHelloHandler}
+ *
+ * @author photowey
+ * @date 2022/02/13
+ * @since 1.0.0
+ */
+@Slf4j
+public class WebsocketHelloHandler extends SimpleChannelInboundHandler<Object> {
+
+    private WebSocketServerHandshaker handShaker;
+
+    private NettyProperties nettyProperties;
+
+    public WebsocketHelloHandler(NettyProperties nettyProperties) {
+        this.nettyProperties = nettyProperties;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        log.info("收到消息：" + msg);
+        if (msg instanceof FullHttpRequest) {
+            // 以http请求形式接入,但是走的是websocket
+            this.handleHttpRequest(ctx, (FullHttpRequest) msg);
+        } else if (msg instanceof WebSocketFrame) {
+            // 处理 websocket 客户端的消息
+            this.handlerWebSocketFrame(ctx, (WebSocketFrame) msg);
+        }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.info("客户端加入连接：" + ctx.channel());
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        // 断开连接
+        log.info("客户端断开连接：" + ctx.channel());
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    private void handlerWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        if (frame instanceof CloseWebSocketFrame) {
+            this.handShaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+        // 示例: 不支持二进制消息
+        if (!(frame instanceof TextWebSocketFrame)) {
+            throw new UnsupportedOperationException(String.format("the %s frame types not supported", frame.getClass().getName()));
+        }
+        String message = ((TextWebSocketFrame) frame).text();
+        log.info("receive message：" + message);
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        TextWebSocketFrame tws = new TextWebSocketFrame(formatter.format(now) + ":" + ctx.channel().id() + "：" + message);
+        ctx.channel().writeAndFlush(tws);
+    }
+
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+        if (!req.decoderResult().isSuccess() || (!"websocket".equals(req.headers().get("Upgrade")))) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+            return;
+        }
+        String url = String.format("ws://localhost:%s/%s", this.nettyProperties.getPort(), this.nettyProperties.getPath());
+        WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(url, null, false);
+        this.handShaker = wsFactory.newHandshaker(req);
+        if (handShaker == null) {
+            WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
+        } else {
+            this.handShaker.handshake(ctx.channel(), req);
+        }
+    }
+
+    private static void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, DefaultFullHttpResponse res) {
+        if (res.status().code() != 200) {
+            ByteBuf buf = Unpooled.copiedBuffer(res.status().toString(), CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+        }
+        ChannelFuture f = ctx.channel().writeAndFlush(res);
+        if (!HttpUtil.isKeepAlive(req) || res.status().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+}
---
+++ netty-in-action/websocket-netty-in-action/src/main/java/com/photowey/websocket/netty/in/action/netty/server/WebsocketNettyServer.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright © 2021 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.websocket.netty.in.action.netty.server;
+
+import com.photowey.websocket.netty.in.action.netty.handler.WebsocketHelloHandler;
+import com.photowey.websocket.netty.in.action.netty.property.NettyProperties;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * {@code WebsocketNettyServer}
+ *
+ * @author photowey
+ * @date 2022/02/13
+ * @since 1.0.0
+ */
+@Slf4j
+@Component
+public class WebsocketNettyServer implements SmartInitializingSingleton, DisposableBean {
+
+    private final ExecutorService executorService;
+
+    private ServerBootstrap bootstrap;
+
+    private EventLoopGroup boss;
+    private EventLoopGroup worker;
+    private Channel channel;
+
+    @Autowired
+    private NettyProperties nettyProperties;
+
+    public WebsocketNettyServer() {
+        this.executorService = Executors.newSingleThreadExecutor();
+    }
+
+    @Override
+    public void afterSingletonsInstantiated() {
+        this.executorService.execute(this::start);
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        this.shutdown();
+    }
+
+    public void start() {
+        this.boss = new NioEventLoopGroup();
+        this.worker = new NioEventLoopGroup();
+
+        this.bootstrap = new ServerBootstrap();
+        this.bootstrap.group(this.boss, this.worker)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast("http-codec", new HttpServerCodec());
+                        pipeline.addLast("aggergator", new HttpObjectAggregator(65536));
+                        pipeline.addLast("http-chunked", new ChunkedWriteHandler());
+                        pipeline.addLast("websocket-handler",
+                                new WebSocketServerProtocolHandler(nettyProperties.getPath(), null, false, 1024 * 1024 * 3));
+                        pipeline.addLast("hello-handler", new WebsocketHelloHandler(nettyProperties));
+                    }
+                });
+        try {
+            this.channel = this.bootstrap.bind(this.nettyProperties.getPort()).sync().channel();
+            log.info("the netty websocket server started on ports:[{}] (tcp) with websocket path:[{}]", this.nettyProperties.getPort(), this.nettyProperties.getPath());
+            this.channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("the netty  websocket server start failure", e);
+        }
+    }
+
+    public void shutdown() {
+        this.shutdownSafe();
+    }
+
+    private void shutdownSafe() {
+        if (null != channel) {
+            channel.close();
+        }
+        if (null != boss) {
+            boss.shutdownGracefully();
+        }
+        if (null != worker) {
+            worker.shutdownGracefully();
+        }
+    }
+}
