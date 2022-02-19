1bb265a7d14b4494c9a1a42670f6e12f7033f780
photowey <photowey@gmail.com>
2022-02-20 01:03:12 +0800

    chore: add MySQL test case.

---
+++ vertx-in-action/vertx-starter/src/main/java/com/photowey/vertx/starter/domain/entity/Employee.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2022 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.vertx.starter.domain.entity;
+
+import java.time.LocalDateTime;
+
+/**
+ * {@code Employee}
+ *
+ * @author photowey
+ * @date 2021/11/02
+ * @since 1.0.0
+ */
+public class Employee {
+
+    /**
+     * 主键标识
+     */
+    private Long id;
+    /**
+     * 工号
+     */
+    private String employeeNo;
+    /**
+     * 隶属机构标识
+     */
+    private Long orgId;
+    /**
+     * 隶属机构名称
+     */
+    private String orgName;
+    /**
+     * 序号
+     */
+    private Integer orderNo;
+    /**
+     * 状态
+     */
+    private Integer status;
+    /**
+     * 备注
+     */
+    private String remark;
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+    /**
+     * 创建人
+     */
+    private Long createBy;
+    /**
+     * 更新时间
+     */
+    private LocalDateTime modifiedTime;
+    /**
+     * 更新人
+     */
+    private Long updateBy;
+    /**
+     * 删除标记
+     */
+    private Integer deleted;
+}
---
+++ vertx-in-action/vertx-starter/src/main/java/com/photowey/vertx/starter/mysql/App.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright © 2022 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.vertx.starter.mysql;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.mysqlclient.MySQLPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.Tuple;
+
+/**
+ * {@code App}
+ *
+ * @author photowey
+ * @date 2022/02/20
+ * @since 1.0.0
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.exceptionHandler(Throwable::printStackTrace).deployVerticle(new MysqlVerticle());
+    }
+
+    public static class MysqlVerticle extends AbstractVerticle {
+
+        /**
+         * <pre>
+         * {
+         *   "id" : 1494377100172378113,
+         *   "employee_no" : "2021109527Meta",
+         *   "org_id" : 2021109527,
+         *   "org_name" : "宇宙漫游者Meta",
+         *   "order_no" : 1024,
+         *   "status" : 1,
+         *   "remark" : "我是备注Meta",
+         *   "gmt_create" : "2022-02-18T02:24",
+         *   "create_by" : "UserInfoMeta",
+         *   "gmt_modified" : "2022-02-18T02:24",
+         *   "update_by" : "UserInfoMeta",
+         *   "deleted" : 1
+         * }
+         * </pre>
+         *
+         * @param startPromise {@link Promise<Void> }
+         * @throws Exception
+         */
+        @Override
+        public void start(Promise<Void> startPromise) throws Exception {
+
+            MySQLConnectOptions connectOptions = new MySQLConnectOptions();
+            connectOptions.setHost("192.168.0.11").setPort(3307).setUser("root").setPassword("root").setDatabase("vertx-in-action")
+                .setCharset("utf8mb4").setUseAffectedRows(true);
+
+            PoolOptions poolOptions = new PoolOptions();
+            poolOptions.setMaxSize(4).setMaxWaitQueueSize(-1);
+
+            MySQLPool mySQLPool = MySQLPool.pool(vertx, connectOptions, poolOptions);
+
+            mySQLPool.getConnection()
+                .compose(connection -> connection.preparedQuery("SELECT * FROM sys_employee WHERE id = ?").execute(Tuple.of(1494377100172378113L)).onComplete(handler -> connection.close()))
+                .onSuccess(rs -> {
+                    for (Row row : rs) {
+                        System.out.println("the index of column is: \n" + row.toJson().encodePrettily());
+                    }
+                }).onFailure(Throwable::printStackTrace);
+        }
+    }
+
+}
