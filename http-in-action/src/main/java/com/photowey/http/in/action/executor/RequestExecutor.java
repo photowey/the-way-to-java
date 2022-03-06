965201bf8daf58c5e262beb6c245910919086474
photowey <photowey@gmail.com>
2022-03-06 14:06:47 +0800

    feat: add file header and optimize error imports.

--- http-in-action/src/main/java/com/photowey/http/in/action/executor/RequestExecutor.java
+++ http-in-action/src/main/java/com/photowey/http/in/action/executor/RequestExecutor.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright © 2021 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.photowey.http.in.action.executor;
 
 import com.photowey.http.in.action.context.RequestContext;
+import com.photowey.http.in.action.enums.HttpMethod;
 import com.photowey.http.in.action.query.RequestHeaders;
 import com.photowey.http.in.action.query.RequestParameters;
 import okhttp3.MediaType;
@@ -43,12 +59,11 @@ public interface RequestExecutor extends ApplicationContextAware {
 
     String doPost(String url, String body, RequestHeaders requestHeaders);
 
-
     // --------------------------------------------------------- combine
 
     /**
      * 聚合请求
-     * 支持 {@link com.uphicoo.cloud.platform.http.enums.HttpMethod} 多类请求
+     * 支持 {@link HttpMethod} 多类请求
      *
      * @param context {@code RequestContext}
      * @return 响应字符串
