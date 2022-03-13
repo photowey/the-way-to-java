681089ec23e879fd4d6c7f4d3b7cec415e05ef44
photowey <photowey@gmail.com>
2022-03-13 15:37:50 +0800

    feat: add netty server and client simple-case.

---
+++ netty-in-action/study-netty-in-action/src/main/java/com/photowey/study/netty/in/action/netty/server/NettyServer.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright © 2021 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.study.netty.in.action.netty.server;
+
+import com.photowey.study.netty.in.action.netty.server.initializer.NettyServerInitializer;
+import com.photowey.study.netty.in.action.netty.server.lisenter.ChannelFutureListenerImpl;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * {@code NettyServer}
+ *
+ * @author photowey
+ * @date 2022/03/13
+ * @since 1.0.0
+ */
+@Slf4j
+public class NettyServer {
+
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new NettyServerInitializer());
+
+            ChannelFuture channelFuture = bootstrap.bind(6888).sync();
+            channelFuture.addListener(new ChannelFutureListenerImpl());
+
+            // listener close channel
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            log.error("start the netty server exception", e);
+            throw new RuntimeException(e);
+        } finally {
+            shutdownGracefully(bossGroup, workerGroup);
+        }
+    }
+
+    private static void shutdownGracefully(EventLoopGroup bossGroup, EventLoopGroup workerGroup) {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+
+}
---
+++ netty-in-action/study-netty-in-action/src/main/java/com/photowey/study/netty/in/action/netty/server/handler/NettyServerHandler.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2021 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.study.netty.in.action.netty.server.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * {@code NettyServerHandler}
+ *
+ * @author photowey
+ * @date 2022/03/13
+ * @since 1.0.0
+ */
+@Slf4j
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        log.info("the remote client:[{}] request message is:[{}]", ctx.channel().remoteAddress(), buf.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Hello, 世界~", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error("channel read exception", cause);
+        ctx.close();
+    }
+}
