4ccbdbb1f7edc38661bb030145d67946af180d07
photowey <photowey@gmail.com>
2022-03-16 00:53:18 +0800

    feat: add AES/CBC/PKCS5Padding .

--- crypto-in-action/src/main/java/com/photowey/crypto/in/action/crypto/CryptoJava.java
+++ crypto-in-action/src/main/java/com/photowey/crypto/in/action/crypto/CryptoJava.java
@@ -24,6 +24,7 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayOutputStream;
 import java.io.Serializable;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.interfaces.RSAPrivateKey;
@@ -41,6 +42,8 @@ import java.util.Map;
  */
 public final class CryptoJava {
 
+    private final static char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
     private CryptoJava() {
         throw new AssertionError("No " + this.getClass().getName() + " instances for you!");
     }
@@ -49,6 +52,7 @@ public final class CryptoJava {
 
         private static final String AES_ALGORITHM = "AES";
         private static final String AES_CBC_NO_PADDING = "AES/CBC/NoPadding";
+        private static final String AES_CBC_PKCS5_PADDING = "AES/CBC/PKCS5Padding";
 
         private AES() {
             throw new AssertionError("No " + this.getClass().getName() + " instances for you!");
@@ -61,55 +65,123 @@ public final class CryptoJava {
             }
 
             public static String encrypt(String rules, String context, String ivString) {
-                return AES.encrypt(rules, context, ivString, AES_CBC_NO_PADDING);
+                return encrypt(rules, context, ivString, AES_CBC_NO_PADDING);
             }
 
             public static String decrypt(String rules, String encrypted, String ivString) {
-                return AES.decrypt(rules, encrypted, ivString, AES_CBC_NO_PADDING);
+                return decrypt(rules, encrypted, ivString, AES_CBC_NO_PADDING);
             }
-        }
 
-        public static String encrypt(String secretKey, String context, String ivString, String cipherString) {
-            byte[] iv = ivString.getBytes(StandardCharsets.UTF_8);
-            try {
-                Cipher cipher = Cipher.getInstance(cipherString);
-                int blockSize = cipher.getBlockSize();
-                byte[] dataBytes = context.getBytes(StandardCharsets.UTF_8);
-                int length = dataBytes.length;
-                if (length % blockSize != 0) {
-                    length = length + (blockSize - (length % blockSize));
+            public static String encrypt(String secretKey, String context, String ivString, String cipherString) {
+                byte[] iv = ivString.getBytes(StandardCharsets.UTF_8);
+                try {
+                    Cipher cipher = Cipher.getInstance(cipherString);
+                    int blockSize = cipher.getBlockSize();
+                    byte[] dataBytes = context.getBytes(StandardCharsets.UTF_8);
+                    int length = dataBytes.length;
+                    if (length % blockSize != 0) {
+                        length = length + (blockSize - (length % blockSize));
+                    }
+                    byte[] plainText = new byte[length];
+                    System.arraycopy(dataBytes, 0, plainText, 0, dataBytes.length);
+
+                    SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), AES_ALGORITHM);
+                    IvParameterSpec ivSpec = new IvParameterSpec(iv);
+                    cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+                    byte[] encrypted = cipher.doFinal(plainText);
+
+                    return Base64Utils.encrypt(encrypted);
+
+                } catch (Exception e) {
+                    throw new SecurityException(String.format("handle aes:[%s] encrypt exception", cipherString), e);
                 }
-                byte[] plainText = new byte[length];
-                System.arraycopy(dataBytes, 0, plainText, 0, dataBytes.length);
+            }
 
-                SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), AES_ALGORITHM);
-                IvParameterSpec ivSpec = new IvParameterSpec(iv);
-                cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
-                byte[] encrypted = cipher.doFinal(plainText);
+            public static String decrypt(String secretKey, String encrypted, String ivString, String cipherString) {
+                byte[] iv = ivString.getBytes(StandardCharsets.UTF_8);
+                try {
+                    byte[] dataBytes = Base64Utils.decrypt(encrypted);
+                    Cipher cipher = Cipher.getInstance(cipherString);
+                    SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), AES_ALGORITHM);
+                    IvParameterSpec ivSpec = new IvParameterSpec(iv);
+                    cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+                    byte[] original = cipher.doFinal(dataBytes);
 
-                return Base64Utils.encrypt(encrypted);
-
-            } catch (Exception e) {
-                throw new SecurityException(String.format("handle aes:[%s] encrypt exception", cipherString), e);
+                    // 为什么? 还需要 再次 trim() -> 可能会有空格
+                    return new String(original, StandardCharsets.UTF_8).trim();
+                } catch (Exception e) {
+                    throw new SecurityException(String.format("handle aes:[%s] decrypt exception", cipherString), e);
+                }
             }
         }
 
-        public static String decrypt(String secretKey, String encrypted, String ivString, String cipherString) {
-            byte[] iv = ivString.getBytes(StandardCharsets.UTF_8);
-            try {
-                byte[] dataBytes = Base64Utils.decrypt(encrypted);
-                Cipher cipher = Cipher.getInstance(cipherString);
-                SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), AES_ALGORITHM);
-                IvParameterSpec ivSpec = new IvParameterSpec(iv);
-                cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
-                byte[] original = cipher.doFinal(dataBytes);
+        public static final class PKCS5Padding implements Serializable {
 
-                // 为什么? 还需要 再次 trim() -> 可能会有空格
-                return new String(original, StandardCharsets.UTF_8).trim();
-            } catch (Exception e) {
-                throw new SecurityException(String.format("handle aes:[%s] decrypt exception", cipherString), e);
+            private static final int INIT_VECTOR_LENGTH = 16;
+
+            public static String encrypt(String secretKey, String context) {
+                return encrypt(secretKey, context, AES_CBC_PKCS5_PADDING);
+            }
+
+            public static String encrypt(String secretKey, String context, String cipherString) {
+                if (!isKeyLengthValid(secretKey)) {
+                    throw new RuntimeException("secret key's length must be 128, 192 or 256 bits");
+                }
+                try {
+                    SecureRandom secureRandom = new SecureRandom();
+                    byte[] iv = new byte[INIT_VECTOR_LENGTH / 2];
+                    secureRandom.nextBytes(iv);
+                    String initVector = bytesToHex(iv);
+                    iv = initVector.getBytes(StandardCharsets.UTF_8);
+
+                    IvParameterSpec ivSpec = new IvParameterSpec(iv);
+                    SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), AES_ALGORITHM);
+
+                    Cipher cipher = Cipher.getInstance(cipherString);
+                    cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+                    byte[] plainText = context.getBytes(StandardCharsets.UTF_8);
+                    byte[] encrypted = cipher.doFinal(plainText);
+
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(iv.length + encrypted.length);
+                    byteBuffer.put(iv);
+                    byteBuffer.put(encrypted);
+
+                    return Base64Utils.encrypt(byteBuffer.array());
+                } catch (Throwable e) {
+                    throw new SecurityException(String.format("handle aes:[%s] encrypt exception", cipherString), e);
+                }
+            }
+
+            public static String decrypt(String secretKey, String cipherText) {
+                return decrypt(secretKey, cipherText, AES_CBC_PKCS5_PADDING);
+            }
+
+            public static String decrypt(String secretKey, String encrypted, String cipherString) {
+                if (!isKeyLengthValid(secretKey)) {
+                    throw new RuntimeException("secret key's length must be 128, 192 or 256 bits");
+                }
+                try {
+                    byte[] cipherText = Base64Utils.decrypt(encrypted);
+
+                    IvParameterSpec ivSpec = new IvParameterSpec(cipherText, 0, INIT_VECTOR_LENGTH);
+                    SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), AES_ALGORITHM);
+
+                    Cipher cipher = Cipher.getInstance(cipherString);
+                    cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+
+                    byte[] data = cipher.doFinal(cipherText, INIT_VECTOR_LENGTH, cipherText.length - INIT_VECTOR_LENGTH);
+
+                    return new String(data, StandardCharsets.UTF_8);
+                } catch (Throwable e) {
+                    throw new SecurityException(String.format("handle aes:[%s] decrypt exception", cipherString), e);
+                }
+            }
+
+            public static boolean isKeyLengthValid(String key) {
+                return key.length() == 16 || key.length() == 24 || key.length() == 32;
             }
         }
+
     }
 
     public static final class RSA implements Serializable {
@@ -375,5 +447,15 @@ public final class CryptoJava {
             return Base64Utils.encrypt(key.getEncoded());
         }
 
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
