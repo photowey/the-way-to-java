8a507664a95038b9b46cd45df3fc69d82f61809f
photowey <photowey@gmail.com>
2022-04-23 15:39:05 +0800

    feat: add fastjson2-in-action

---
+++ fastjson2-in-action/src/main/java/com/photowey/fastjson2/in/action/domain/Product.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2021 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.fastjson2.in.action.domain;
+
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * {@code Product}
+ *
+ * @author photowey
+ * @date 2022/04/23
+ * @since 1.0.0
+ */
+@ToString
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = -6899900509665415646L;
+
+    public Long id;
+    public String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Product id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Product name(String name) {
+        this.name = name;
+        return this;
+    }
+}
---
+++ fastjson2-in-action/src/test/java/com/photowey/fastjson2/in/action/domain/ProductTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright © 2021 photowey (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.fastjson2.in.action.domain;
+
+import com.alibaba.fastjson2.*;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * {@code ProductTest}
+ *
+ * @author photowey
+ * @date 2022/04/23
+ * @since 1.0.0
+ */
+class ProductTest {
+
+    @Test
+    void testJson() {
+        Product product = new Product();
+        product.setId(1010L);
+        product.setName("DataWorks");
+
+        String jsonString = JSON.toJSONString(product);
+        System.out.println(jsonString);
+
+        String toJSONString = JSON.toJSONString(product, JSONWriter.Feature.BeanToArray);
+        System.out.println(toJSONString);
+
+        Product product2 = JSON.parseObject(jsonString, Product.class);
+        System.out.println(product2);
+
+        byte[] jsonbBytes = JSONB.toBytes(product);
+
+        Product product3 = JSONB.parseObject(jsonbBytes, Product.class);
+        System.out.println(product3);
+    }
+
+
+    @Test
+    void testJsonBytes() {
+        Product product = new Product();
+        product.setId(1010L);
+        product.setName("DataWorks");
+
+        byte[] jsonbBytes = JSONB.toBytes(product);
+
+        Product product2 = JSONB.parseObject(jsonbBytes, Product.class);
+        System.out.println(product2);
+
+        byte[] utf8Bytes = "{\"id\":1010,\"name\":\"DataWorks\"}".getBytes(StandardCharsets.UTF_8);
+        Product product3 = JSON.parseObject(utf8Bytes, Product.class);
+        System.out.println(product3);
+    }
+
+    @Test
+    void testJsonParse() {
+        Product product = new Product();
+        product.setId(1010L);
+        product.setName("DataWorks");
+
+        String jsonString = JSON.toJSONString(product);
+        System.out.println(jsonString);
+
+        Product product2 = JSON.parseObject(jsonString, Product.class);
+        System.out.println(product2);
+    }
+
+    @Test
+    void testJsonPath() {
+        String str = "{\"id\":1010,\"name\":\"DataWorks\"}";
+
+        JSONPath path = JSONPath.of("$.id"); // 缓存起来重复使用能提升性能
+        JSONReader parser = JSONReader.of(str);
+        Object result = path.extract(parser);
+        System.out.println(result);
+
+        byte[] utf8Bytes = str.getBytes(StandardCharsets.UTF_8);
+
+        JSONReader parser2 = JSONReader.of(utf8Bytes);
+        Object result2 = path.extract(parser2);
+        System.out.println(result2);
+    }
+}
