76770d7a74fee33a5a00c20bf14a05b2d9c9d071
photowey <photowey@gmail.com>
2022-11-19 23:30:48 +0800

    feat: 1.Upgrade pom;2.refresh license header

--- redis-in-action/src/main/resources/lock/r_lock.lua
+++ redis-in-action/src/main/resources/lock/r_lock.lua
@@ -1,3 +1,20 @@
+--[[
+
+    Copyright © 2021 the original author or authors (photowey@gmail.com)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+]]
 --- 读锁定时,锁状态置为1,不做任何阻塞
 --- 写锁定时, 锁状态置为2, 阻塞其他读写
 --- 无锁时,锁状态为0,或者不存在该 key
