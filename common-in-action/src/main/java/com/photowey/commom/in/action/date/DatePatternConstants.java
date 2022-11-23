cdacb08c99d0c9577fe18e2e60a00def25bd9ad3
photowey <photowey@gmail.com>
2022-11-23 20:45:51 +0800

    feat: 1.Add date utility class

---
+++ common-in-action/src/main/java/com/photowey/commom/in/action/date/DatePatternConstants.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2021 the original author or authors (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.commom.in.action.date;
+
+/**
+ * {@code DatePatternConstants}
+ *
+ * @author photowey
+ * @date 2022/11/23
+ * @since 1.0.0
+ */
+public interface DatePatternConstants {
+
+    String yyyy_MM_dd = "yyyy-MM-dd ";
+    String HH_mm_ss = "HH:mm:ss";
+    String yyyyMMdd = "yyyyMMdd";
+    String HHmmss = "HHmmss";
+
+    String yyyy_MM_dd_HH_mm_ss = "yyyy-MM-dd HH:mm:ss";
+    String yyyy_MM_dd_HH_mm_ss_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
+
+    String yyyyMMddHHmmss = "yyyyMMddHHmmss";
+    String yyyyMMddHHmmssSSS = "yyyyMMddHHmmssSSS";
+
+}
