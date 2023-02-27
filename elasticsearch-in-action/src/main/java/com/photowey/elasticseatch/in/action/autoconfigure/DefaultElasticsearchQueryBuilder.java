9fa98210cc86078d357d6fd9e0a369ce1add1cdc
photowey <photowey@gmail.com>
2023-02-27 23:43:59 +0800

    feat: 1.Update-ElasticsearchQueryBuilder

--- elasticsearch-in-action/src/main/java/com/photowey/elasticseatch/in/action/autoconfigure/DefaultElasticsearchQueryBuilder.java
+++ elasticsearch-in-action/src/main/java/com/photowey/elasticseatch/in/action/autoconfigure/DefaultElasticsearchQueryBuilder.java
@@ -15,13 +15,22 @@
  */
 package com.photowey.elasticseatch.in.action.autoconfigure;
 
+import com.photowey.commom.in.action.util.ObjectUtils;
+import com.photowey.elasticseatch.in.action.constant.ElasticsearchQueryConstants;
+import com.photowey.elasticseatch.in.action.enums.MathOperatorEnum;
 import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 
 import java.util.Collection;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -46,58 +55,150 @@ public class DefaultElasticsearchQueryBuilder implements ElasticsearchQueryBuild
 
     @Override
     public <V> QueryBuilder nestedQuery(String path, String field, V v, ScoreMode mode) {
-        QueryBuilder termQuery = QueryBuilders.termQuery(String.format("%s.%s", path, field), v);
+        QueryBuilder termQuery = QueryBuilders.termQuery(this.formatPath(path, field), v);
 
         return this.nestedQuery(path, termQuery, mode);
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+
     @Override
     public <V> QueryBuilder nestedRangeGtQuery(String path, String field, V v, ScoreMode mode) {
-        String fieldx = String.format("%s.%s", path, field);
-        QueryBuilder termQuery = QueryBuilders.rangeQuery(fieldx).gt(v);
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).gt(v);
 
         return this.nestedQuery(path, termQuery, mode);
     }
 
     @Override
     public <V> QueryBuilder nestedRangeLtQuery(String path, String field, V v, ScoreMode mode) {
-        String fieldx = String.format("%s.%s", path, field);
-        QueryBuilder termQuery = QueryBuilders.rangeQuery(fieldx).lt(v);
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).lt(v);
 
         return this.nestedQuery(path, termQuery, mode);
     }
 
     @Override
     public <V> QueryBuilder nestedRangeXtQuery(String path, String field, V v1, V v2, ScoreMode mode) {
-        String fieldx = String.format("%s.%s", path, field);
-        QueryBuilder termQuery = QueryBuilders.rangeQuery(fieldx).gt(v1).lt(v2);
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).gt(v1).lt(v2);
 
         return this.nestedQuery(path, termQuery, mode);
     }
 
     @Override
     public <V> QueryBuilder nestedRangeGteQuery(String path, String field, V v, ScoreMode mode) {
-        String fieldx = String.format("%s.%s", path, field);
-        QueryBuilder termQuery = QueryBuilders.rangeQuery(fieldx).gte(v);
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).gte(v);
 
         return this.nestedQuery(path, termQuery, mode);
     }
 
     @Override
     public <V> QueryBuilder nestedRangeLteQuery(String path, String field, V v, ScoreMode mode) {
-        String fieldx = String.format("%s.%s", path, field);
-        QueryBuilder termQuery = QueryBuilders.rangeQuery(fieldx).lte(v);
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).lte(v);
 
         return this.nestedQuery(path, termQuery, mode);
     }
 
     @Override
     public <V> QueryBuilder nestedRangeXteQuery(String path, String field, V v1, V v2, ScoreMode mode) {
-        String fieldx = String.format("%s.%s", path, field);
-        QueryBuilder termQuery = QueryBuilders.rangeQuery(fieldx).gte(v1).lte(v2);
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).gte(v1).lte(v2);
 
         return this.nestedQuery(path, termQuery, mode);
     }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public QueryBuilder filterQuery(QueryBuilder query) {
+        return QueryBuilders.boolQuery().filter(query);
+    }
+
+    @Override
+    public QueryBuilder shouldQuery(QueryBuilder query) {
+        return QueryBuilders.boolQuery().should(query);
+    }
+
+    @Override
+    public QueryBuilder mustQuery(QueryBuilder query) {
+        return QueryBuilders.boolQuery().must(query);
+    }
+
+    @Override
+    public QueryBuilder mustNotQuery(QueryBuilder query) {
+        return QueryBuilders.boolQuery().mustNot(query);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public <V> QueryBuilder rangeGtQuery(String path, String field, V v) {
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).gt(v);
+
+        return this.mustQuery(termQuery);
+    }
+
+    @Override
+    public <V> QueryBuilder rangeLtQuery(String path, String field, V v) {
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).lt(v);
+
+        return this.mustQuery(termQuery);
+    }
+
+    @Override
+    public <V> QueryBuilder rangeXtQuery(String path, String field, V v1, V v2) {
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).gt(v1).lt(v2);
+
+        return this.mustQuery(termQuery);
+    }
+
+    @Override
+    public <V> QueryBuilder rangeGteQuery(String path, String field, V v) {
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).gte(v);
+
+        return this.mustQuery(termQuery);
+    }
+
+    @Override
+    public <V> QueryBuilder rangeLteQuery(String path, String field, V v) {
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).lte(v);
+
+        return this.mustQuery(termQuery);
+    }
+
+    @Override
+    public <V> QueryBuilder rangeXteQuery(String path, String field, V v1, V v2) {
+        QueryBuilder termQuery = QueryBuilders.rangeQuery(this.formatPath(path, field)).gte(v1).lte(v2);
+
+        return this.mustQuery(termQuery);
+    }
+
+    @Override
+    public <V> QueryBuilder rangeQuery(String path, String field, V v, MathOperatorEnum operator) {
+        switch (operator) {
+            case GT:
+                return this.rangeGtQuery(field, v);
+            case LT:
+                return this.rangeLtQuery(field, v);
+            case GTE:
+                return this.rangeGteQuery(field, v);
+            case LTE:
+                return this.rangeLteQuery(field, v);
+        }
+
+        throw new UnsupportedOperationException("UnSupported operator");
+    }
+
+    @Override
+    public <V> QueryBuilder rangeQuery(String path, String field, V v1, V v2, MathOperatorEnum operator) {
+        switch (operator) {
+            case GT_LT:
+                return this.rangeXtQuery(path, field, v1, v2);
+            case GTE_LTE:
+                return this.rangeXteQuery(path, field, v1, v2);
+        }
+
+        throw new UnsupportedOperationException("UnSupported operator");
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
 
     @Override
     public <V, CV> QueryBuilder nestedCascadeQuery(String path, String field, V v, String cascadeField, CV cv) {
@@ -106,8 +207,8 @@ public class DefaultElasticsearchQueryBuilder implements ElasticsearchQueryBuild
 
     @Override
     public <V, CV> QueryBuilder nestedCascadeQuery(String path, String field, V v, String cascadeField, CV cv, ScoreMode mode) {
-        QueryBuilder termQuery = QueryBuilders.termQuery(String.format("%s.%s", path, field), v);
-        QueryBuilder termQueryCascade = QueryBuilders.termQuery(String.format("%s.%s", path, cascadeField), cv);
+        QueryBuilder termQuery = QueryBuilders.termQuery(this.formatPath(path, field), v);
+        QueryBuilder termQueryCascade = QueryBuilders.termQuery(this.formatPath(path, cascadeField), cv);
         QueryBuilder termQueryx = QueryBuilders.boolQuery().filter(termQuery).filter(termQueryCascade);
 
         return this.nestedQuery(path, termQueryx, mode);
@@ -115,7 +216,7 @@ public class DefaultElasticsearchQueryBuilder implements ElasticsearchQueryBuild
 
     @Override
     public <V> QueryBuilder nestedMatchQuery(String path, String field, V v, ScoreMode mode) {
-        QueryBuilder matchQuery = QueryBuilders.matchQuery(String.format("%s.%s", path, field), v);
+        QueryBuilder matchQuery = QueryBuilders.matchQuery(String.format(ElasticsearchQueryConstants.TESTED_PATH_TEMPLATE, path, field), v);
 
         return this.nestedQuery(path, matchQuery, mode);
     }
@@ -148,6 +249,18 @@ public class DefaultElasticsearchQueryBuilder implements ElasticsearchQueryBuild
         return QueryBuilders.nestedQuery(path, boolQuery, mode);
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public FieldSortBuilder idSort() {
+        return this.pkSort();
+    }
+
+    @Override
+    public FieldSortBuilder pkSort() {
+        return this.fieldSort(ElasticsearchQueryConstants.FIELD_ID_STRING);
+    }
+
     @Override
     public FieldSortBuilder fieldSort(String field) {
         return this.fieldSort(field, SortOrder.DESC);
@@ -171,5 +284,75 @@ public class DefaultElasticsearchQueryBuilder implements ElasticsearchQueryBuild
     @Override
     public Collection<FieldSortBuilder> fieldSortDesc(String... fields) {
         return Stream.of(fields).map(this::fieldSort).collect(Collectors.toSet());
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public NativeSearchQuery pageableQuery(QueryBuilder builder, int pageNo, int pageSize) {
+        return this.pageableQuery(builder, pageNo, pageSize, (b) -> {
+        });
+    }
+
+    @Override
+    public NativeSearchQuery pageableQuery(QueryBuilder builder, int pageNo, int pageSize, Consumer<NativeSearchQueryBuilder> fx) {
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder()
+                .withQuery(builder)
+                .withSorts(this.pkSort())
+                .withPageable(PageRequest.of(pageNo, pageSize));
+
+        fx.accept(queryBuilder);
+
+        return queryBuilder.build();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public HighlightBuilder.Field highlightField(String field) {
+        return new HighlightBuilder.Field(field);
+    }
+
+    @Override
+    public HighlightBuilder highlightBuilder() {
+        return SearchSourceBuilder
+                .highlight()
+                .preTags(ElasticsearchQueryConstants.HIGHLIGHT_PRE_TAG)
+                .postTags(ElasticsearchQueryConstants.HIGHLIGHT_POST_TAG);
+    }
+
+    @Override
+    public NativeSearchQuery highlightQuery(QueryBuilder builder, String field) {
+        return this.highlightPageableQuery(builder, field, 0, 0, false);
+    }
+
+    @Override
+    public NativeSearchQuery highlightPageableQuery(QueryBuilder builder, String field, int pageNo, int pageSize) {
+        return this.highlightPageableQuery(builder, field, pageNo, pageSize, true);
+    }
+
+    @Override
+    public NativeSearchQuery highlightPageableQuery(QueryBuilder builder, String field, int pageNo, int pageSize, boolean pageable) {
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder()
+                .withQuery(builder)
+                .withHighlightFields(this.highlightField(field))
+                .withHighlightBuilder(this.highlightBuilder())
+                .withSorts(this.pkSort());
+
+        if (pageable) {
+            nativeSearchQueryBuilder.withPageable(PageRequest.of(pageNo, pageNo));
+        }
+
+        return nativeSearchQueryBuilder.build();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private String formatPath(String path, String field) {
+        if (ObjectUtils.isNullOrEmpty(path)) {
+            return field;
+        }
+
+        return String.format(ElasticsearchQueryConstants.TESTED_PATH_TEMPLATE, path, field);
     }
 }
--- elasticsearch-in-action/src/main/java/com/photowey/elasticseatch/in/action/autoconfigure/ElasticsearchQueryBuilder.java
+++ elasticsearch-in-action/src/main/java/com/photowey/elasticseatch/in/action/autoconfigure/ElasticsearchQueryBuilder.java
@@ -15,12 +15,17 @@
  */
 package com.photowey.elasticseatch.in.action.autoconfigure;
 
+import com.photowey.elasticseatch.in.action.enums.MathOperatorEnum;
 import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 
 import java.util.Collection;
+import java.util.function.Consumer;
 
 /**
  * {@code ElasticsearchQueryBuilder}
@@ -30,11 +35,15 @@ import java.util.Collection;
  * @since 1.0.0
  */
 public interface ElasticsearchQueryBuilder {
+
     <V> QueryBuilder nestedQuery(String path, String field, V v);
+
 
     <V> QueryBuilder nestedMatchQuery(String path, String field, V v);
 
     <V> QueryBuilder nestedQuery(String path, String field, V v, ScoreMode mode);
+
+    // -----------------------------------------------------------------------------------------------------------------
 
     <V> QueryBuilder nestedRangeGtQuery(String path, String field, V v, ScoreMode mode);
 
@@ -47,6 +56,62 @@ public interface ElasticsearchQueryBuilder {
     <V> QueryBuilder nestedRangeLteQuery(String path, String field, V v, ScoreMode mode);
 
     <V> QueryBuilder nestedRangeXteQuery(String path, String field, V v1, V v2, ScoreMode mode);
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    QueryBuilder filterQuery(QueryBuilder query);
+
+    QueryBuilder shouldQuery(QueryBuilder query);
+
+    QueryBuilder mustQuery(QueryBuilder query);
+
+    QueryBuilder mustNotQuery(QueryBuilder query);
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    default <V> QueryBuilder rangeGtQuery(String field, V v) {
+        return this.rangeGtQuery("", field, v);
+    }
+
+    default <V> QueryBuilder rangeLtQuery(String field, V v) {
+        return this.rangeLtQuery("", field, v);
+    }
+
+    default <V> QueryBuilder rangeXtQuery(String field, V v1, V v2) {
+        return this.rangeXtQuery("", field, v1, v2);
+    }
+
+    default <V> QueryBuilder rangeGteQuery(String field, V v) {
+        return this.rangeGteQuery("", field, v);
+    }
+
+    default <V> QueryBuilder rangeLteQuery(String field, V v) {
+        return this.rangeLteQuery("", field, v);
+    }
+
+    default <V> QueryBuilder rangeXteQuery(String field, V v1, V v2) {
+        return this.rangeXteQuery("", field, v1, v2);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    <V> QueryBuilder rangeGtQuery(String path, String field, V v);
+
+    <V> QueryBuilder rangeLtQuery(String path, String field, V v);
+
+    <V> QueryBuilder rangeXtQuery(String path, String field, V v1, V v2);
+
+    <V> QueryBuilder rangeGteQuery(String path, String field, V v);
+
+    <V> QueryBuilder rangeLteQuery(String path, String field, V v);
+
+    <V> QueryBuilder rangeXteQuery(String path, String field, V v1, V v2);
+
+    <V> QueryBuilder rangeQuery(String path, String field, V v, MathOperatorEnum operator);
+
+    <V> QueryBuilder rangeQuery(String path, String field, V v1, V v2, MathOperatorEnum operator);
+
+    // -----------------------------------------------------------------------------------------------------------------
 
     <V, CV> QueryBuilder nestedCascadeQuery(String path, String field, V v, String cascadeField, CV cv);
 
@@ -62,6 +127,10 @@ public interface ElasticsearchQueryBuilder {
 
     QueryBuilder nestedShouldQuery(String path, QueryBuilder query, ScoreMode mode);
 
+    FieldSortBuilder idSort();
+
+    FieldSortBuilder pkSort();
+
     FieldSortBuilder fieldSort(String field);
 
     FieldSortBuilder fieldSort(String field, SortOrder order);
@@ -72,4 +141,21 @@ public interface ElasticsearchQueryBuilder {
 
     Collection<FieldSortBuilder> fieldSortDesc(String... field);
 
+    // -----------------------------------------------------------------------------------------------------------------
+
+    NativeSearchQuery pageableQuery(QueryBuilder builder, int pageNo, int pageSize);
+
+    NativeSearchQuery pageableQuery(QueryBuilder builder, int pageNo, int pageSize, Consumer<NativeSearchQueryBuilder> fx);
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    HighlightBuilder.Field highlightField(String field);
+
+    HighlightBuilder highlightBuilder();
+
+    NativeSearchQuery highlightQuery(QueryBuilder builder, String field);
+
+    NativeSearchQuery highlightPageableQuery(QueryBuilder builder, String field, int pageNo, int pageSize);
+
+    NativeSearchQuery highlightPageableQuery(QueryBuilder builder, String field, int pageNo, int pageSize, boolean pageable);
 }
