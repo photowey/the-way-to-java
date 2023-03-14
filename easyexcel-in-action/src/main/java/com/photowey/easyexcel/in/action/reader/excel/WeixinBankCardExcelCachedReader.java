418d0f55e24ffee10216c7803376a18a0c1e5ed2
photowey <photowey@gmail.com>
2023-03-14 22:45:18 +0800

    feat: 1.Update: remove package

--- easyexcel-in-action/src/main/java/com/photowey/easyexcel/in/action/reader/excel/WeixinBankCardExcelCachedReader.java
+++ easyexcel-in-action/src/main/java/com/photowey/easyexcel/in/action/reader/excel/WeixinBankCardExcelCachedReader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.photowey.easyexcel.in.action.reader;
+package com.photowey.easyexcel.in.action.reader.excel;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.read.listener.ReadListener;
--- easyexcel-in-action/src/main/java/com/photowey/easyexcel/in/action/reader/resource/ResourceReader.java
+++ easyexcel-in-action/src/main/java/com/photowey/easyexcel/in/action/reader/resource/ResourceReader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.photowey.easyexcel.in.action.reader;
+package com.photowey.easyexcel.in.action.reader.resource;
 
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.io.ClassPathResource;
