06ee14171a104d1b3f01ec5d3d33d258dcbdd4b1
photowey <photowey@gmail.com>
2023-03-19 22:52:00 +0800

    feat: 1.Add: easyexcel data converters

---
+++ easyexcel-in-action/src/main/java/com/photowey/easyexcel/in/action/converter/GenderIntegerWrite2StringConverter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2021 the original author or authors (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.easyexcel.in.action.converter;
+
+import com.alibaba.excel.converters.Converter;
+import com.alibaba.excel.converters.WriteConverterContext;
+import com.alibaba.excel.enums.CellDataTypeEnum;
+import com.alibaba.excel.metadata.data.WriteCellData;
+
+/**
+ * {@code GenderIntegerWrite2StringConverter}
+ *
+ * @author photowey
+ * @date 2023/03/19
+ * @since 1.0.0
+ */
+public class GenderIntegerWrite2StringConverter implements Converter<Integer> {
+
+    @Override
+    public Class<?> supportJavaTypeKey() {
+        return Integer.class;
+    }
+
+    @Override
+    public CellDataTypeEnum supportExcelTypeKey() {
+        return CellDataTypeEnum.STRING;
+    }
+
+    @Override
+    public WriteCellData<?> convertToExcelData(WriteConverterContext<Integer> context) throws Exception {
+        Integer value = context.getValue();
+        // 0:未知,1:男,2:女
+        String txt = 0 == value ? "未知" : 1 == value ? "男" : "女";
+
+        return new WriteCellData<>(txt);
+    }
+}
---
+++ easyexcel-in-action/src/main/java/com/photowey/easyexcel/in/action/converter/IntegerWrite2StringConverter.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2021 the original author or authors (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.easyexcel.in.action.converter;
+
+import com.alibaba.excel.converters.Converter;
+import com.alibaba.excel.converters.WriteConverterContext;
+import com.alibaba.excel.enums.CellDataTypeEnum;
+import com.alibaba.excel.metadata.data.WriteCellData;
+
+/**
+ * {@code IntegerWrite2StringConverter}
+ *
+ * @author photowey
+ * @date 2023/03/19
+ * @since 1.0.0
+ */
+public class IntegerWrite2StringConverter implements Converter<Integer> {
+
+    @Override
+    public Class<?> supportJavaTypeKey() {
+        return Integer.class;
+    }
+
+    @Override
+    public CellDataTypeEnum supportExcelTypeKey() {
+        return CellDataTypeEnum.STRING;
+    }
+
+    @Override
+    public WriteCellData<?> convertToExcelData(WriteConverterContext<Integer> context) throws Exception {
+        Integer value = context.getValue();
+        return new WriteCellData<>(String.valueOf(value));
+    }
+}
