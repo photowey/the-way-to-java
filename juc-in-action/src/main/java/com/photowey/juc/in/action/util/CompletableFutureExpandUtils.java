3d69cd14f79a684b7490c0f970aae2765cb5e897
photowey <photowey@gmail.com>
2023-04-02 12:41:23 +0800

    feat: 1.Add: CompletableFuture util

---
+++ juc-in-action/src/main/java/com/photowey/juc/in/action/util/CompletableFutureExpandUtils.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright © 2021 the original author or authors (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.juc.in.action.util;
+
+import java.util.concurrent.*;
+import java.util.function.BiConsumer;
+
+/**
+ * {@code CompletableFutureExpandUtils}
+ *
+ * <pre>
+ * 作者：京东云开发者
+ *     链接：https://juejin.cn/post/7197606993858281530
+ *     来源：稀土掘金
+ *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ * </pre>
+ *
+ * @author photowey
+ * @date 2023/04/02
+ * @since 1.0.0
+ */
+public class CompletableFutureExpandUtils {
+
+    /**
+     * 如果在给定超时之前未完成，则异常完成此 CompletableFuture 并抛出 {@link TimeoutException} 。
+     *
+     * @param timeout 在出现 TimeoutException 异常完成之前等待多长时间，以 {@code unit} 为单位
+     * @param unit    一个 {@link TimeUnit}，结合 {@code timeout} 参数，表示给定粒度单位的持续时间
+     * @return 入参的 CompletableFuture
+     */
+    public static <T> CompletableFuture<T> orTimeout(CompletableFuture<T> future, long timeout, TimeUnit unit) {
+        if (null == unit) {
+            throw new UncheckedException("时间的给定粒度不能为空");
+        }
+        if (null == future) {
+            throw new UncheckedException("异步任务不能为空");
+        }
+        if (future.isDone()) {
+            return future;
+        }
+
+        return future.whenComplete(new Canceller(Delayer.delay(new Timeout(future), timeout, unit)));
+    }
+
+    /**
+     * 超时时异常完成的操作
+     */
+    static final class Timeout implements Runnable {
+        final CompletableFuture<?> future;
+
+        Timeout(CompletableFuture<?> future) {
+            this.future = future;
+        }
+
+        public void run() {
+            if (null != future && !future.isDone()) {
+                future.completeExceptionally(new TimeoutException());
+            }
+        }
+    }
+
+    /**
+     * 取消不需要的超时的操作
+     */
+    static final class Canceller implements BiConsumer<Object, Throwable> {
+        final Future<?> future;
+
+        Canceller(Future<?> future) {
+            this.future = future;
+        }
+
+        public void accept(Object ignore, Throwable ex) {
+            if (null == ex && null != future && !future.isDone()) {
+                future.cancel(false);
+            }
+        }
+    }
+
+    /**
+     * 单例延迟调度器，仅用于启动和取消任务，一个线程就足够
+     */
+    static final class Delayer {
+
+        static final ScheduledThreadPoolExecutor delayer;
+
+        static {
+            delayer = new ScheduledThreadPoolExecutor(1, new DaemonThreadFactory());
+            delayer.setRemoveOnCancelPolicy(true);
+        }
+
+        static ScheduledFuture<?> delay(Runnable command, long delay, TimeUnit unit) {
+            return delayer.schedule(command, delay, unit);
+        }
+
+        static final class DaemonThreadFactory implements ThreadFactory {
+
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                t.setName("CompletableFutureExpandUtilsDelayScheduler");
+                return t;
+            }
+        }
+    }
+
+    public static class UncheckedException extends RuntimeException {
+
+        public UncheckedException(String message) {
+            super(message);
+        }
+    }
+}
+
