89a816b8755641f434e38613491e98f69ba10b6a
photowey <photowey@gmail.com>
2023-05-27 00:00:39 +0800

    feat: 1.Update: Knife4j starter 配置

--- knife4j-in-action/src/main/java/com/photowey/knife4j/in/action/config/Knife4jAutoConfigure.java
+++ knife4j-in-action/src/main/java/com/photowey/knife4j/in/action/config/Knife4jAutoConfigure.java
@@ -15,10 +15,36 @@
  */
 package com.photowey.knife4j.in.action.config;
 
+import com.github.xiaoymin.knife4j.spring.extension.OpenApiExtensionResolver;
+import com.google.common.net.HttpHeaders;
 import com.photowey.knife4j.in.action.properties.Knife4jStarterProperties;
 import com.photowey.swagger.provider.ext.annotation.EnableSwaggerProviderExt;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+import org.springframework.util.ClassUtils;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * {@code Knife4jAutoConfigure}
@@ -29,6 +55,153 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableSwaggerProviderExt
+@Import(BeanValidatorPluginsConfiguration.class)
 @EnableConfigurationProperties(value = {Knife4jStarterProperties.class})
-public class Knife4jAutoConfigure {
+public class Knife4jAutoConfigure implements EnvironmentAware {
+
+    @Autowired(required = false)
+    private OpenApiExtensionResolver openApiExtensionResolver;
+    @Autowired
+    private Knife4jStarterProperties knife4JProperties;
+
+    private Environment environment;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean("autoDocket")
+    @ConditionalOnMissingBean(Docket.class)
+    public Docket autoDocket() {
+        Knife4jStarterProperties.Project project = this.knife4JProperties.getProject();
+        Knife4jStarterProperties.Api api = this.knife4JProperties.getApi();
+        String group = api.getGroup();
+        List<String> controllers = api.getControllers();
+        Contact contact = new Contact(
+                project.getAuthor(),
+                "https://wiki.photowey.dev/developer?auther=" + project.getAuthor(),
+                project.getEmail()
+        );
+        Docket docket = new Docket(DocumentationType.OAS_30)
+                .enable(this.docEnabled())
+                .apiInfo(this.populateApiInfo(contact))
+                // Support timestamp
+                .directModelSubstitute(LocalDateTime.class, Long.class)
+                .groupName(group)
+                .select()
+                .apis(basePackages(controllers))
+                .paths(PathSelectors.any())
+                .build();
+        if (null != this.openApiExtensionResolver) {
+            docket.extensions(this.openApiExtensionResolver.buildExtensions(group));
+        }
+        if (api.isEnableSecurity()) {
+            docket.securitySchemes(this.securitySchemes())
+                    .securityContexts(this.securityContexts());
+        }
+
+        // this.determineGlobalRequestParameters(docket);
+
+        return docket;
+    }
+
+    private void determineGlobalRequestParameters(Docket docket) {
+        List<RequestParameter> parameters = this.determineParameters();
+        if (ObjectUtils.isNotEmpty(parameters)) {
+            docket.globalRequestParameters(parameters);
+        }
+    }
+
+    private ApiInfo populateApiInfo(Contact contact) {
+        Knife4jStarterProperties.Api api = this.knife4JProperties.getApi();
+        return new ApiInfoBuilder()
+                .title(ObjectUtils.defaultIfNull(api.getTitle(), String.format("%s project", this.appName())))
+                .description(ObjectUtils.defaultIfNull(api.getDescription(), String.format("# %s project RESTful APIs", this.appName())))
+                .termsOfServiceUrl(ObjectUtils.defaultIfNull(api.getTermsOfServiceUrl(), "https://wiki.photowey.dev/"))
+                .contact(contact)
+                .version(ObjectUtils.defaultIfNull(api.getVersion(), "1.0.0"))
+                .build();
+    }
+
+    private String pathMapping(String app) {
+        return "/" + app;
+    }
+
+    public boolean docEnabled() {
+        return !Boolean.parseBoolean(this.environment.getProperty("knife4j.production"));
+    }
+
+    private String appName() {
+        return this.environment.getProperty("spring.application.name");
+    }
+
+    private static Predicate<RequestHandler> basePackages(final List<String> basePackages) {
+        return (input) -> {
+            return (Boolean) declaringClass(input).map(handlerPackages(basePackages)).orElse(true);
+        };
+    }
+
+    private static Optional<Class<?>> declaringClass(RequestHandler input) {
+        return Optional.ofNullable(input.declaringClass());
+    }
+
+    private static Function<Class<?>, Boolean> handlerPackages(final List<String> basePackages) {
+        return (input) -> {
+
+            for (String basePackage : basePackages) {
+                if (ClassUtils.getPackageName(input).startsWith(basePackage)) {
+                    return true;
+                }
+            }
+
+            return false;
+
+        };
+    }
+
+    // ----------------------------------------------------------------
+
+    private List<SecurityScheme> securitySchemes() {
+        List<SecurityScheme> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey(HttpHeaders.AUTHORIZATION, HttpHeaders.AUTHORIZATION, io.swagger.v3.oas.models.security.SecurityScheme.In.HEADER.name());
+        result.add(apiKey);
+        return result;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> contexts = new ArrayList<>();
+        Knife4jStarterProperties.Api api = this.knife4JProperties.getApi();
+        api.getPathRegexes().forEach((pathRegex) -> contexts.add(this.populateContext(pathRegex)));
+
+        return contexts;
+    }
+
+    private SecurityContext populateContext(String pathRegex) {
+        return SecurityContext
+                .builder()
+                .securityReferences(defaultReferences())
+                .operationSelector(ctx -> ctx.requestMappingPattern().matches(pathRegex))
+                .build();
+    }
+
+    private List<SecurityReference> defaultReferences() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{new AuthorizationScope("global", "accessEverything")};
+        List<SecurityReference> securityReferences = new ArrayList<>();
+        securityReferences.add(new SecurityReference(HttpHeaders.AUTHORIZATION, authorizationScopes));
+        return securityReferences;
+    }
+
+    private List<RequestParameter> determineParameters() {
+        RequestParameterBuilder builder = new RequestParameterBuilder();
+        List<RequestParameter> pars = new ArrayList<>();
+        builder.name("x-ut").description("认证令牌")
+                .in(ParameterType.HEADER)
+                .required(false)
+                .build();
+        pars.add(builder.build());
+
+        return pars;
+
+    }
 }
--- knife4j-in-action/src/main/java/com/photowey/knife4j/in/action/properties/Knife4jStarterProperties.java
+++ knife4j-in-action/src/main/java/com/photowey/knife4j/in/action/properties/Knife4jStarterProperties.java
@@ -17,8 +17,13 @@ package com.photowey.knife4j.in.action.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * {@code Knife4jStarterProperties}
@@ -33,5 +38,36 @@ public class Knife4jStarterProperties implements Serializable {
 
     private static final long serialVersionUID = -7992540227934302975L;
 
-    private String appDocket;
+    private Project project = new Project();
+    private Api api = new Api();
+
+    @Data
+    @Validated
+    public static class Project implements Serializable {
+
+        private static final long serialVersionUID = -1249689015297696151L;
+
+        private String name;
+        @NotBlank(message = "请配置: `knife4j` 文档作者: `author`")
+        private String author;
+        @NotBlank(message = "请配置: `knife4j` 文档作者邮箱: `email`")
+        private String email;
+    }
+
+    @Data
+    @Validated
+    public static class Api implements Serializable {
+
+        private static final long serialVersionUID = 3534129045736770485L;
+
+        private String group;
+        private String title;
+        private String description;
+        private String termsOfServiceUrl = "https://wiki.photowey.dev/photowey";
+        private String version = "1.0.0";
+        private boolean enableSecurity = false;
+        @NotEmpty(message = "请配置: `knife4j` 文档控制器列表: `controllers`")
+        private List<String> controllers = new ArrayList<>();
+        private List<String> pathRegexes = new ArrayList<>();
+    }
 }
