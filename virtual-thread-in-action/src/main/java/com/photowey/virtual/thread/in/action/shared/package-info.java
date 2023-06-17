64cad875a296869d13918e969fa577b316dceea0
photowey <photowey@gmail.com>
2023-06-17 15:04:42 +0800

    feat: Add Spring 6.1.0-M1 VirtualThreadTaskExecutor

---
+++ virtual-thread-in-action/src/main/java/com/photowey/virtual/thread/in/action/shared/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * {@code com.photowey.virtual.thread.in.action.shared}
+ * 复制的 Spring 6.1.0-M1
+ *
+ * @author photowey
+ * @date 2023/06/17
+ * @since 1.0.0
+ */
+package com.photowey.virtual.thread.in.action.shared;
