1a722ed38c60b6d5d13133714ce680e71940774a
photowey <photowey@gmail.com>
2023-07-07 00:10:55 +0800

    feat: 1.Add: trim blank-space for string field

---
+++ spring-in-action/src/main/java/com/photowey/spring/in/action/aop/TrimAspect.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright © 2021 the original author or authors (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.spring.in.action.aop;
+
+import com.photowey.spring.in.action.annotation.TrimField;
+import com.photowey.spring.in.action.domain.model.TrimModel;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+/**
+ * {@code TrimAspect}
+ *
+ * @author photowey
+ * @date 2023/07/06
+ * @since 1.0.0
+ */
+@Slf4j
+@Aspect
+@Component
+public class TrimAspect {
+
+    @Pointcut(value = "@annotation(com.photowey.spring.in.action.annotation.TrimMethod)")
+    public void trimMethodPointcut() {
+
+    }
+
+    @Around("trimMethodPointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        for (int i = 0; i < args.length; i++) {
+            Object arg = args[i];
+            this.handleTrimModel(joinPoint, arg);
+            boolean isTargetField = this.handleTrimStringModel(joinPoint, i, arg);
+            if (isTargetField) {
+                args[i] = String.valueOf(arg).replaceAll(" ", "");
+            }
+        }
+
+        return joinPoint.proceed(args);
+    }
+
+    private void handleTrimModel(ProceedingJoinPoint joinPoint, Object arg) {
+        if (arg instanceof TrimModel) {
+            ReflectionUtils.doWithFields(arg.getClass(), (field) -> {
+                ReflectionUtils.makeAccessible(field);
+                Object pv = field.get(arg);
+                if (pv instanceof String) {
+                    field.set(arg, String.valueOf(arg).replaceAll(" ", ""));
+                }
+            }, (f) -> f.isAnnotationPresent(TrimField.class));
+        }
+    }
+
+    private boolean handleTrimStringModel(ProceedingJoinPoint joinPoint, int index, Object arg) {
+        if (arg instanceof String) {
+            Method method = this.determineRequestMethod(joinPoint);
+            Parameter[] parameters = method.getParameters();
+            Parameter parameter = parameters[index];
+
+            return parameter.isAnnotationPresent(TrimField.class);
+        }
+
+        return false;
+    }
+
+    private Method determineRequestMethod(ProceedingJoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        return signature.getMethod();
+    }
+}
---
+++ spring-in-action/src/main/java/com/photowey/spring/in/action/controller/TrimController.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright © 2021 the original author or authors (photowey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.spring.in.action.controller;
+
+import com.photowey.spring.in.action.annotation.TrimField;
+import com.photowey.spring.in.action.annotation.TrimMethod;
+import com.photowey.spring.in.action.domain.model.TrimModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+
+/**
+ * {@code TrimController}
+ *
+ * @author photowey
+ * @date 2023/07/06
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/trim")
+@Api(value = "参数修改", tags = {"String参数修改"})
+public class TrimController {
+
+    @TrimMethod
+    @GetMapping("/get")
+    public ResponseEntity<String> get(@TrimField @RequestParam("name") String name) {
+        return new ResponseEntity<>(String.format("hello %s~", name), HttpStatus.OK);
+    }
+
+    @TrimMethod
+    @PostMapping("/post")
+    public ResponseEntity<String> post(@RequestBody @ApiParam @Validated StudentPayload payload) {
+        return new ResponseEntity<>(String.format("hello %s~", payload.getName()), HttpStatus.OK);
+    }
+
+    @TrimMethod
+    @GetMapping("/get/query")
+    public ResponseEntity<String> queryGet(StudentQuery payload) {
+        return new ResponseEntity<>(String.format("hello %s~", payload.getName()), HttpStatus.OK);
+    }
+
+    @Data
+    public static class StudentPayload implements Serializable, TrimModel {
+
+        private static final long serialVersionUID = 3067875834364794883L;
+
+        @TrimField
+        private String name;
+    }
+
+    @Data
+    public static class StudentQuery implements Serializable, TrimModel {
+
+        private static final long serialVersionUID = 3067875834364794883L;
+
+        @TrimField
+        private String name;
+    }
+}
