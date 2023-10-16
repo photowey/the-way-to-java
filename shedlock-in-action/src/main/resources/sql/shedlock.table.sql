74fa320568ebf7d645afee81eab70a3f0b60fb46
photowey <photowey@gmail.com>
2023-10-17 00:43:45 +0800

    feat: Add: 1.shedlock-in-action module

---
+++ shedlock-in-action/src/main/resources/sql/shedlock.table.sql
@@ -0,0 +1,19 @@
+
+-- MySQL
+CREATE TABLE `shedlock` (
+  `name` varchar(64) NOT NULL,
+  `lock_until` timestamp(3) NULL DEFAULT NULL,
+  `locked_at` timestamp(3) NULL DEFAULT NULL,
+  `locked_by` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- PostgreSQL
+--drop table if exist shedlock;
+--create table shedlock (
+--   name                 VARCHAR(64)          not null,
+--   lock_until           TIMESTAMP            null,
+--   locked_at            TIMESTAMP            null,
+--   locked_by            VARCHAR(255)         null,
+--   constraint PK_SHEDLOCK primary key (name)
+--);
