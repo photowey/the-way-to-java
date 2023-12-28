6f439cd781669c4b67da6f977b00840efe0d0db0
photowey <photowey@gmail.com>
2023-12-28 23:44:19 +0800

    feat: Add 1.: Lambda function

---
+++ common-in-action/src/main/java/com/photowey/common/in/action/func/lambda/LambdaFunction.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.common.in.action.func.lambda;
+
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
+import java.util.Locale;
+import java.util.function.Function;
+
+/**
+ * {@code LambdaFunction}
+ *
+ * @author photowey
+ * @date 2023/12/28
+ * @since 1.0.0
+ */
+@FunctionalInterface
+public interface LambdaFunction<T, R> extends Function<T, R> {
+
+    static <T, R> String resolve(LambdaFunction<T, R> fx) {
+        try {
+            Method method = fx.getClass().getDeclaredMethod("writeReplace");
+            method.setAccessible(Boolean.TRUE);
+            SerializedLambda serializedLambda = (SerializedLambda) method.invoke(fx);
+            String getter = serializedLambda.getImplMethodName();
+            return methodToProperty(getter);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * copy from {@code org.apache.ibatis.reflection.property.PropertyNamer}
+     */
+    static String methodToProperty(String name) {
+        if (name.startsWith("is")) {
+            name = name.substring(2);
+        } else if (name.startsWith("get") || name.startsWith("set")) {
+            name = name.substring(3);
+        } else {
+            throw new RuntimeException("Error parsing property name '" + name + "'.  Didn't start with 'is', 'get' or 'set'.");
+        }
+
+        if (name.length() == 1 || (name.length() > 1 && !Character.isUpperCase(name.charAt(1)))) {
+            name = name.substring(0, 1).toLowerCase(Locale.ENGLISH) + name.substring(1);
+        }
+
+        return name;
+    }
+}
