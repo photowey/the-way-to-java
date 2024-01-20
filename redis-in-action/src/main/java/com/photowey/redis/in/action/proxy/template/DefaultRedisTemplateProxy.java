d9a862ec2cfe1e900a2c714b2928cf3e38a98c4c
photowey <photowey@gmail.com>
2024-01-20 23:53:20 +0800

    feat: Add 1: Default RedisTemplateProxy

---
+++ redis-in-action/src/main/java/com/photowey/redis/in/action/proxy/template/DefaultRedisTemplateProxy.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.redis.in.action.proxy.template;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * {@code DefaultRedisTemplateProxy}
+ *
+ * @author photowey
+ * @date 2024/01/20
+ * @since 1.0.0
+ */
+public class DefaultRedisTemplateProxy implements RedisTemplateProxy {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public DefaultRedisTemplateProxy(
+            RedisTemplate<String, Object> redisTemplate,
+            StringRedisTemplate stringRedisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public RedisTemplate<String, Object> redis() {
+        return this.redisTemplate;
+    }
+
+    @Override
+    public StringRedisTemplate stringRedis() {
+        return this.stringRedisTemplate;
+    }
+}
