4d1629820760284bd5ec45b5e83b6b7d24ae9c34
photowey <photowey@gmail.com>
2024-02-11 00:54:56 +0800

    docs: Refresh 1: header/error copy

---
+++ common-in-action/src/main/java/com/photowey/common/in/action/promise/Promise.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.common.in.action.promise;
+
+import java.util.function.Consumer;
+
+/**
+ * {@code Promise}
+ *
+ * @author photowey
+ * @date 2024/02/10
+ * @since 1.0.0
+ */
+public final class Promise<T, E extends Throwable> {
+
+    private static final Promise<?, ? extends Throwable> EMPTY = new Promise<>();
+
+    public static <T, E extends Throwable> Promise<T, E> valueOf(T value) {
+        if (null == value) {
+            return (Promise<T, E>) Promise.EMPTY;
+        }
+
+        return new Promise<>(value);
+    }
+
+    public static <T, E extends Throwable> Promise<T, E> throwableOf(E throwable) {
+        if (null == throwable) {
+            return (Promise<T, E>) Promise.EMPTY;
+        }
+
+        return new Promise<>(throwable);
+    }
+
+    private final T value;
+    private final E throwable;
+
+    public Promise() {
+        this(null, null);
+    }
+
+    public Promise(T value) {
+        this(value, null);
+    }
+
+    public Promise(E throwable) {
+        this(null, throwable);
+    }
+
+    public Promise(T value, E throwable) {
+        this.value = value;
+        this.throwable = throwable;
+    }
+
+    public boolean determineIsPresent() {
+        return !this.determineIsEmpty();
+    }
+
+    public boolean determineIsEmpty() {
+        return this.value == null;
+    }
+
+    public boolean determineIsFailed() {
+        return this.throwable != null;
+    }
+
+    public Promise<T, E> then(Consumer<T> resolve) {
+        if (this.determineIsPresent()) {
+            resolve.accept(this.value);
+        }
+
+        return this;
+    }
+
+    public Promise<T, E> then(Consumer<T> resolve, Consumer<T> reject) {
+        if (this.determineIsPresent()) {
+            resolve.accept(this.value);
+            return this;
+        }
+
+        reject.accept(this.value);
+
+        return this;
+    }
+
+    public Promise<T, E> then(Consumer<T> resolve, Consumer<T> reject, Consumer<E> throwable) {
+        if (this.determineIsFailed()) {
+            throwable.accept(this.throwable);
+            return this;
+        }
+
+        if (this.determineIsPresent()) {
+            resolve.accept(this.value);
+            return this;
+        }
+
+        reject.accept(this.value);
+
+        return this;
+    }
+
+    public Promise<T, E> throwable(Consumer<E> throwable) {
+        if (this.determineIsFailed()) {
+            throwable.accept(this.throwable);
+        }
+
+        return this;
+    }
+
+    public T value() {
+        return this.value;
+    }
+}
---
+++ common-in-action/src/test/java/com/photowey/common/in/action/promise/PromiseTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.common.in.action.promise;
+
+import lombok.Data;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+
+/**
+ * {@code PromiseTest}
+ *
+ * @author photowey
+ * @date 2024/02/11
+ * @since 1.0.0
+ */
+class PromiseTest {
+
+    @Data
+    public static class Message implements Serializable {
+
+        private static final long serialVersionUID = -7142353781081408528L;
+
+        private String value;
+
+        public Message(String value) {
+            this.value = value;
+        }
+    }
+
+    @Test
+    void testPromise_then_resolve() {
+        Promise<Message, Throwable> promise = Promise.valueOf(null);
+        promise.then(Assertions::assertNull);
+    }
+
+    @Test
+    void testPromise_then__resolve_reject() {
+        Promise<PromiseTest.Message, Throwable> promise = Promise.valueOf(new PromiseTest.Message("ok"));
+        promise.then((value) -> {
+            Assertions.assertNotNull(value);
+            Assertions.assertEquals("ok", value.getValue());
+        }, (x) -> {
+            throw new UnsupportedOperationException("Unsupported now");
+        });
+    }
+
+    @Test
+    void testPromise_throwable() {
+        Promise<PromiseTest.Message, Throwable> promise = Promise.throwableOf(new RuntimeException("failed"));
+        promise.throwable(Assertions::assertNotNull);
+    }
+
+    @Test
+    void testPromise_then_throwable() {
+        Promise<PromiseTest.Message, Throwable> promise = Promise.throwableOf(new RuntimeException("failed"));
+        promise.then((value) -> {
+            throw new UnsupportedOperationException("Unsupported now");
+        }).throwable(Assertions::assertNotNull);
+    }
+
+    @Test
+    void testPromise_then__resolve_reject_throwable() {
+        Promise<PromiseTest.Message, Throwable> promise = Promise.throwableOf(new RuntimeException("failed"));
+        promise.then((v1) -> {
+            throw new UnsupportedOperationException("Unsupported now");
+        }, (v2) -> {
+            throw new UnsupportedOperationException("Unsupported now");
+        }, Assertions::assertNotNull);
+    }
+
+    @Test
+    void testPromise_then__value() {
+        Promise<PromiseTest.Message, Throwable> promise = Promise.valueOf(new PromiseTest.Message("ok"));
+        Message message = promise.then((value) -> {
+            Assertions.assertNotNull(value);
+            Assertions.assertEquals("ok", value.getValue());
+        }).value();
+
+        Assertions.assertNotNull(message);
+        Assertions.assertEquals("ok", message.getValue());
+    }
+
+    @Test
+    void testPromise_throwable__value() {
+        Promise<PromiseTest.Message, Throwable> promise = Promise.throwableOf(new RuntimeException("failed"));
+        Message message = promise.throwable(Assertions::assertNotNull).value();
+
+        Assertions.assertNull(message);
+    }
+}
