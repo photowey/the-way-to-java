5b5de6a716a7995fec3ba570e4084d259fbe9d79
photowey <photowey@gmail.com>
2024-03-12 22:14:39 +0800

    feat: Add 1: CompositeRedissonDelayedQueue
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ redisson-delayed-queue-in-action/src/main/java/io/github/photowey/redisson/delayed/queue/in/action/getter/ApplicationContextGetter.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.redisson.delayed.queue.in.action.getter;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * {@code ApplicationContextGetter}
+ *
+ * @author photowey
+ * @date 2024/03/12
+ * @since 1.0.0
+ */
+public interface ApplicationContextGetter {
+    ApplicationContext applicationContext();
+}
---
+++ redisson-delayed-queue-in-action/src/main/java/io/github/photowey/redisson/delayed/queue/in/action/queue/CompositeRedissonDelayedQueue.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.redisson.delayed.queue.in.action.queue;
+
+import io.github.photowey.redisson.delayed.queue.in.action.core.pair.QueuePair;
+import io.github.photowey.redisson.delayed.queue.in.action.core.task.RedissonDelayedTask;
+import io.github.photowey.redisson.delayed.queue.in.action.manager.RedissonDelayedQueueManager;
+import io.github.photowey.redisson.delayed.queue.in.action.property.RedissonClientProperties;
+import org.redisson.api.RDelayedQueue;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+
+import java.io.Serializable;
+
+/**
+ * {@code CompositeRedissonDelayedQueue}
+ *
+ * @author photowey
+ * @date 2024/03/12
+ * @since 1.0.0
+ */
+public class CompositeRedissonDelayedQueue implements RedissonDelayedQueue {
+
+    private final RedissonDelayedQueueManager manager;
+    private ListableBeanFactory beanFactory;
+
+    public CompositeRedissonDelayedQueue(RedissonDelayedQueueManager manager) {
+        this.manager = manager;
+
+        this.manager.register(this);
+    }
+
+    // ----------------------------------------------------------------
+
+    @Override
+    public RedissonDelayedQueueManager manager() {
+        return this.manager;
+    }
+
+    // ----------------------------------------------------------------
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = (ListableBeanFactory) beanFactory;
+    }
+
+    @Override
+    public RedissonClientProperties redissonProperties() {
+        return this.manager.redissonProperties();
+    }
+
+    // ----------------------------------------------------------------
+
+    @Override
+    public String topic() {
+        return this.redissonProperties().getDelayed().getTopic();
+    }
+
+    @Override
+    public <P extends Serializable> void offer(RedissonDelayedTask<P> task) {
+        QueuePair pair = this.manager().tryAcquirePair(this.topic());
+        RDelayedQueue<RedissonDelayedTask<?>> delayedQueue = pair.delayedQueue();
+
+        delayedQueue.offer(task, task.getDelayed(), task.determineTimeUnit());
+    }
+}
