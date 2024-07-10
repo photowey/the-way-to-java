fdb2428445c7034f12ec1f1e34cacdd409b72c26
photowey <photowey@gmail.com>
2024-07-10 21:34:03 +0800

    feat: Add: 1.object-pool-in-action module.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ object-pool-in-action/src/test/java/com/photowey/com/photowey/object/pool/in/action/AppTests.java
@@ -0,0 +1,18 @@
+package com.photowey.com.photowey.object.pool.in.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * {@code AppTests}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2024/07/10
+ */
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {}
+}
