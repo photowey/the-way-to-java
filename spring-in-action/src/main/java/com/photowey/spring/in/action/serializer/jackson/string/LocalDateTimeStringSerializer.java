832c6c97723c634ae9b1a18aed3367c2ddb4097e
photowey <photowey@gmail.com>
2025-01-04 21:27:10 +0800

    feat: Add: LocalDateTime Serializer/Deserializer.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ spring-in-action/src/main/java/com/photowey/spring/in/action/serializer/jackson/string/LocalDateTimeStringSerializer.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.spring.in.action.serializer.jackson.string;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.photowey.common.in.action.date.v2.LocalDateTimeTransformer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * {@code LocalDateTimeStringSerializer}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2024/12/16
+ */
+public class LocalDateTimeStringSerializer extends JsonSerializer<LocalDateTime> {
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (value != null) {
+            String datetime = LocalDateTimeTransformer.format(value);
+            gen.writeString(datetime);
+        }
+    }
+}
