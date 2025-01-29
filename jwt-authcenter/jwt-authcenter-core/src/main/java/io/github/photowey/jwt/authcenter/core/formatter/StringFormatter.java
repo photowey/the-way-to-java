565a7d813d40c3e1ada47c2336191fc07b6d1595
photowey <photowey@gmail.com>
2025-01-29 17:51:41 +0800

    feat: Add: 1.String formatter.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ jwt-authcenter/jwt-authcenter-core/src/main/java/io/github/photowey/jwt/authcenter/core/formatter/StringFormatter.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.jwt.authcenter.core.formatter;
+
+import io.github.photowey.shaded.org.slf4j.helpers.FormattingTuple;
+import io.github.photowey.shaded.org.slf4j.helpers.MessageFormatter;
+
+/**
+ * {@code StringFormatter}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2025/01/29
+ */
+public interface StringFormatter {
+
+    static String format(String messagePattern, Object... args) {
+        if (null == messagePattern) {
+            return messagePattern;
+        }
+
+        FormattingTuple formattingTuple = MessageFormatter.arrayFormat(messagePattern, args);
+        return formattingTuple.getMessage();
+    }
+}
+
---
+++ jwt-authcenter/jwt-authcenter-core/src/main/java/io/github/photowey/shaded/org/slf4j/helpers/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * {@code io.github.photowey.shaded.org.slf4j.helpers}
+ * copy from {@code org.slf4j.helpers}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2025/01/29
+ */
+package io.github.photowey.shaded.org.slf4j.helpers;
