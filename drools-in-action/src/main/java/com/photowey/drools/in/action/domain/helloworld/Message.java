38485304f239666cf226aaa029e19b253e21cd93
photowey <photowey@gmail.com>
2025-02-09 23:12:09 +0800

    feat: Add: 1.drools HelloWorld ut.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ drools-in-action/src/main/java/com/photowey/drools/in/action/domain/helloworld/Message.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.drools.in.action.domain.helloworld;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * {@code Message}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2025/02/09
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message implements Serializable {
+
+    private static final long serialVersionUID = -2644911088216136292L;
+
+    public static final int HELLO = 0;
+    public static final int GOODBYE = 1;
+
+    private String message;
+
+    private int status;
+
+    public static Message doSomething(Message message) {
+        return message;
+    }
+
+    public boolean isSomething(String msg, List<Object> list) {
+        list.add(this);
+        return this.message.equals(msg);
+    }
+}
---
+++ drools-in-action/src/test/java/com/photowey/drools/in/action/domain/helloworld/MessageTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.drools.in.action.domain.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.kie.api.KieServices;
+import org.kie.api.event.rule.DebugAgendaEventListener;
+import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
+import org.kie.api.logger.KieRuntimeLogger;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import java.util.ArrayList;
+
+/**
+ * {@code MessageTest}
+ *
+ * @author photowey
+ * @since 2025/02/09
+ */
+class MessageTest {
+
+    @Test
+    void testHelloWorld() {
+        KieServices ks = KieServices.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+
+        execute(ks, kc);
+    }
+
+    public static void execute(KieServices ks, KieContainer kc) {
+        KieSession ksession = kc.newKieSession("io.github.photowey.drools.kbase.ksession.001");
+
+        ksession.setGlobal("list", new ArrayList<>());
+
+        ksession.addEventListener(new DebugAgendaEventListener());
+        ksession.addEventListener(new DebugRuleRuntimeEventListener());
+
+        KieRuntimeLogger logger = ks.getLoggers().newFileLogger(ksession, "./helloworld");
+
+        final Message message = new Message();
+        message.setMessage("Hello World");
+        message.setStatus(Message.HELLO);
+
+        ksession.insert(message);
+        ksession.fireAllRules();
+
+        logger.close();
+
+        ksession.dispose();
+    }
+}
