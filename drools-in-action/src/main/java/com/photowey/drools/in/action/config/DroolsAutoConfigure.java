c3103c1840c85d75addcbd7461d565930c4d4f01
photowey <photowey@gmail.com>
2025-02-11 23:43:57 +0800

    feat: Add: 1.Kie 与 Spring 整合.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ drools-in-action/src/main/java/com/photowey/drools/in/action/config/DroolsAutoConfigure.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.drools.in.action.config;
+
+import com.photowey.drools.in.action.bind.PropertyBinder;
+import com.photowey.drools.in.action.property.DroolsProperties;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieRepository;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.spring.KModuleBeanFactoryPostProcessor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * {@code DroolsAutoConfigure}.
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2025/02/11
+ */
+@AutoConfiguration
+@Import(KModuleBeanFactoryPostProcessor.class)
+public class DroolsAutoConfigure {
+
+    @Bean
+    public DroolsProperties droolsProperties(Environment environment) {
+        return PropertyBinder.bind(
+            environment,
+            DroolsProperties.getPrefix(),
+            DroolsProperties.class
+        );
+    }
+
+    // ----------------------------------------------------------------
+
+    @Bean
+    @ConditionalOnMissingBean(KieFileSystem.class)
+    public KieFileSystem kieFileSystem(ResourcePatternResolver resolver, DroolsProperties props) {
+        KieFileSystem kfs = this.kieServices().newKieFileSystem();
+
+        for (Resource ruleFile : this.tryReadRuleFiles(resolver, props)) {
+            String resourcePath = props.populateRuleFullPath(ruleFile.getFilename());
+            org.kie.api.io.Resource kr =
+                ResourceFactory.newClassPathResource(resourcePath, StandardCharsets.UTF_8.displayName());
+            kfs.write(kr);
+        }
+
+        return kfs;
+    }
+
+    // ----------------------------------------------------------------
+
+    @Bean
+    @ConditionalOnMissingBean(KieContainer.class)
+    public KieContainer kieContainer(KieFileSystem kieFileSystem) {
+        final KieRepository kieRepository = this.kieServices().getRepository();
+        kieRepository.addKieModule(kieRepository::getDefaultReleaseId);
+        KieBuilder kieBuilder = this.kieServices().newKieBuilder(kieFileSystem);
+        kieBuilder.buildAll();
+
+        return this.kieServices().newKieContainer(kieRepository.getDefaultReleaseId());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(KieBase.class)
+    public KieBase defaultKieBase(KieFileSystem kieFileSystem) {
+        return this.kieContainer(kieFileSystem).getKieBase();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(KieSession.class)
+    public KieSession defaultKieSession(KieFileSystem kieFileSystem) {
+        return this.kieContainer(kieFileSystem).newKieSession();
+    }
+
+    // ----------------------------------------------------------------
+
+    @Bean
+    @ConditionalOnMissingBean(ResourcePatternResolver.class)
+    public ResourcePatternResolver resourcePatternResolver() {
+        return new PathMatchingResourcePatternResolver();
+    }
+
+    // ----------------------------------------------------------------
+
+    private KieServices kieServices() {
+        return KieServices.get();
+    }
+
+    private Resource[] tryReadRuleFiles(ResourcePatternResolver resolver, DroolsProperties props) {
+        try {
+            return resolver.getResources(props.populateClasspathRuleResources());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ drools-in-action/src/main/java/com/photowey/drools/in/action/property/DroolsProperties.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.drools.in.action.property;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * {@code DroolsProperties}.
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2025/02/11
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DroolsProperties implements Serializable {
+
+    private static final long serialVersionUID = 4479253614243628525L;
+
+    private static final String SLUSH = "/";
+
+    private String rulePath = "io/github/photowey/drools/rules/";
+
+    // ----------------------------------------------------------------
+
+    public String populateClasspathRuleResources() {
+        return String.format("classpath*:%s**/*.*", tryTrim(this.rulePath));
+    }
+
+    public String populateRuleFullPath(String filename) {
+        return tryTrim(this.rulePath) + filename;
+    }
+
+    // ----------------------------------------------------------------
+
+    public static String tryTrim(String path) {
+        String tmp = path;
+        if (tmp.startsWith(SLUSH)) {
+            tmp = tmp.substring(1);
+        }
+
+        if (tmp.endsWith(SLUSH)) {
+            tmp = tmp.substring(0, tmp.length() - 1);
+        }
+
+        return tmp;
+    }
+
+    // ----------------------------------------------------------------
+
+    public static String getPrefix() {
+        return "io.github.photowey.drools.rules";
+    }
+}
